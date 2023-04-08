a95d0178a3068736123bddab2ce8f85f665a0bba
Vidotti <vidottizzz@gmail.com>
2023-04-08 17:48:25 -0300

    add more 1 type of calculating

--- script/script.js
+++ script/script.js
@@ -29,17 +29,49 @@ function calculateDate() {
     let yearDiv = document.querySelector("#yearDiv");
     let monthDiv = document.querySelector("#monthDiv");
     let dayDiv = document.querySelector("#dayDiv");
+// his birthday
+    let datePast = `${year}-${month}-${day}`;
+
+    let yearss = new Date(dateString).getFullYear() - new Date(datePast).getFullYear();
+    let monthss = new Date(dateString).getMonth() - new Date(datePast).getMonth();
+    let dayss = new Date(dateString).getDate() - Number(day);
+
+    if (monthss < 0) {
+      yearss = yearss - 1;
+      monthss = monthss + 12;
+    }
+  
+    if (dayss < 0) {
+      dayss += getNoOfDays(year, month - 1);
+    }
+    // Display Values
+    if (day != "" && month != "" && year != "") {
+    dayDiv.innerHTML = dayss;
+    monthDiv.innerHTML = monthss;
+    yearDiv.innerHTML = yearss;
+    }
+  };
+  // Get Number of Days in a particular months
+  function getNoOfDays(y, m) {
+    return new Date(y, m, 0).getDate();
+  }
+
+  function calculateDiffDate() {
+    let day = document.forms["Form"]["dayInput"].value;
+    let month = document.forms["Form"]["monthInput"].value;
+    let year = document.forms["Form"]["yearInput"].value;
+
+    let yearDiv = document.querySelector("#yearDiv");
+    let monthDiv = document.querySelector("#monthDiv");
+    let dayDiv = document.querySelector("#dayDiv");
 
     let datePast = `${year}-${month}-${day}`;
     const diffInMs = new Date(dateString) - new Date(datePast);
     const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
     const diffInMonth = diffInDays / 30;
     
-    console.log(diffInMs);
 
-
-
-  if (day != "" && month != "" && year != "") {
+    if (day != "" && month != "" && year != "") {
     let yearCalculated = currentYear - year;
     yearDiv.innerHTML = yearCalculated;
 // calculando q quantidade de meses
@@ -48,15 +80,16 @@ function calculateDate() {
 // e a de dias
     // dayDiv.innerHTML = diffInDays.toFixed(0);
     dayDiv.innerHTML = diffInDays.toFixed(0);
-
-
   }
 
-  // let yearCalculated = currentYear - year;
-  // yearDiv.innerHTML = yearCalculated;
-  // console.log(yearCalculated);
+  let yearCalculated = currentYear - year;
+  yearDiv.innerHTML = yearCalculated;
+  console.log(yearCalculated);
 }
 
+
+
+//validade do botão 1 (tempo total)
 function validate() {
   let day = document.forms["Form"]["dayInput"].value;
   let month = document.forms["Form"]["monthInput"].value;
@@ -101,6 +134,54 @@ function validate() {
   }
 
   calculateDate();
+
+}
+
+//validade do botão 2 ( tempo corrido)
+function validate1() {
+  let day = document.forms["Form"]["dayInput"].value;
+  let month = document.forms["Form"]["monthInput"].value;
+  let year = document.forms["Form"]["yearInput"].value;
+  
+
+  let errMsg1 = document.querySelector(".err-msg1");
+  let errMsg2 = document.querySelector(".err-msg2");
+  let errMsg3 = document.querySelector(".err-msg3");
+
+  let label1 = document.querySelector("#label-red");
+  let label2 = document.querySelector("#label-red2");
+  let label3 = document.querySelector("#label-red3");
+
+  // se nome for vazio adiciona a classe vermelha para a borda do elemento  e remove o atributo hide da mensagem de erro
+  if (day == "" || day > 31) {
+    errMsg1.classList.remove("hide");
+    document.forms["Form"]["dayInput"].classList.add("border-red");
+    label1.classList.add("color-red");
+  } else {
+    errMsg1.classList.add("hide");
+    document.forms["Form"]["dayInput"].classList.remove("border-red");
+    label1.classList.remove("color-red");
+  }
+  if (month == "" || month > 12) {
+    errMsg2.classList.remove("hide");
+    document.forms["Form"]["monthInput"].classList.add("border-red");
+    label2.classList.add("color-red");
+  } else {
+    errMsg2.classList.add("hide");
+    document.forms["Form"]["monthInput"].classList.remove("border-red");
+    label2.classList.remove("color-red");
+  }
+  if (year == "" || year > currentYear) {
+    errMsg3.classList.remove("hide");
+    document.forms["Form"]["yearInput"].classList.add("border-red");
+    label3.classList.add("color-red");
+  } else {
+    errMsg3.classList.add("hide");
+    document.forms["Form"]["yearInput"].classList.remove("border-red");
+    label3.classList.remove("color-red");
+  }
+
+  calculateDiffDate();
 
 
 
